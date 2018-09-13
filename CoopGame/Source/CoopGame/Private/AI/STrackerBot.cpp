b29103136db77f4a7fe3a5286d03714981abf1e8
Switch <sweatyelephant@gmail.com>
2018-09-12 21:41:39 -0400

    Started SPickupActor for powerups

--- CoopGame/Source/CoopGame/Private/AI/STrackerBot.cpp
+++ CoopGame/Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -288,6 +288,8 @@ void ASTrackerBot::Tick(float DeltaTime)
 
 void ASTrackerBot::NotifyActorBeginOverlap(AActor * OtherActor)
 {
+	Super::NotifyActorBeginOverlap(OtherActor);
+
 	if (!bStartedSelfDestruction && !bExploded)
 	{
 		ASCharacter* PlayerPawn = Cast<ASCharacter>(OtherActor);
